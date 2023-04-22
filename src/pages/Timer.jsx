2512ff01790f3e2da33fe4cff4d623d97e83e217
Alan Nguyen <alannguyen@Alans-MacBook-Pro.local>
2023-04-21 23:20:04 -0700

    Created pomodoro timer

---
+++ src/pages/Timer.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+
+function Timer() {
+  const [timer, setTimer] = useState(25 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+  const [timerType, setTimerType] = useState('work');
+
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const toggleTimer = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const resetTimer = () => {
+    setIsRunning(false);
+    setTimer(timerType === 'work' ? 25 * 60 : 5 * 60);
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60).toString().padStart(2, '0');
+    const seconds = (time % 60).toString().padStart(2, '0');
+    return `${minutes}:${seconds}`;
+  };
+
+  const handleTimerTypeChange = (e) => {
+    const newTimerType = e.target.value;
+    setTimerType(newTimerType);
+    setIsRunning(false);
+    setTimer(newTimerType === 'work' ? 25 * 60 : 5 * 60);
+  };
+
+  return (
+    <div>
+      <h1>{timerType === 'work' ? 'Work Timer' : 'Break Timer'}</h1>
+      <h2>{formatTime(timer)}</h2>
+      <button type="button" onClick={toggleTimer}>{isRunning ? 'Pause' : 'Start'}</button>
+      <button type="button" onClick={resetTimer}>Reset</button>
+      <div>
+        <label htmlFor="work-timer">
+          <input
+            id="work-timer"
+            type="radio"
+            value="work"
+            checked={timerType === 'work'}
+            onChange={handleTimerTypeChange}
+          />
+          Work Timer
+        </label>
+        <label htmlFor="break-timer">
+          <input
+            id="break-timer"
+            type="radio"
+            value="break"
+            checked={timerType === 'break'}
+            onChange={handleTimerTypeChange}
+          />
+          Break Timer
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default Timer;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable */
 import Example from './Example';
+import Timer from './Timer';
 
 export {
   Example,
+  Timer,
 };
