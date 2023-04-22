5ae979e18026eff80503e59e9cf4145850a62e2e
Jerry Xu <54759887+xu-jerry@users.noreply.github.com>
2023-04-22 16:41:53 -0700

    server :starstruck:

---
+++ server/index.cjs
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const serverRoutes = require('./routes.cjs');
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use('/dopamind', serverRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is listening on port:${PORT}`));
