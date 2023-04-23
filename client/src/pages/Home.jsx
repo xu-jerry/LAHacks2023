517c9d442325701711ba8650b33209ef591623c8
Juliet Zhang <julietzhang3@gmail.com>
2023-04-22 20:26:08 -0700

    Fix home page styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,8 +4,10 @@ import styles from '../styles/Home.module.css';
 function Home() {
   return (
     <div className={styles.home}>
-      <div>Rewiring your brain for success.</div>
-      <div>Our comprehensive web app for students with ADHD is designed to help improve their focus, organization, and social skills. Our app is an all-in-one solution for students with ADHD, providing them with fun and engaging activities that are tailored to their unique needs.</div>
+      <div className={styles.home1}>
+        <h1>Rewiring your brain for success.</h1>
+        <p>Our comprehensive web app for students with ADHD is designed to help improve their focus, organization, and social skills. Our app is an all-in-one solution for students with ADHD, providing them with fun and engaging activities that are tailored to their unique needs.</p>
+      </div>
     </div>
   );
 }
