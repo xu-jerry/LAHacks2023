6e83f454dd14161c49c0c754d2183648f23e040d
Juliet Zhang <julietzhang3@gmail.com>
2023-04-22 21:22:33 -0700

    Roughly style games cards

--- client/src/pages/Games.jsx
+++ client/src/pages/Games.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import Sudoku from '../components/Sudoku';
-import Anagrams from '../components/Anagrams';
+import { Link } from 'react-router-dom';
+import styles from '../styles/Games.module.css';
 
 function Games() {
   return (
-    <div>
-      <div>
-        Sudoku:
-        {' '}
-        <Sudoku />
-      </div>
-      <div>
-        Anagrams:
-        {' '}
-        <Anagrams />
+    <div className={styles.games}>
+      <div className={styles.cards_container}>
+        <Link to="/games/sudoku">
+          <div className={styles.card}>
+            <img src="https://reactjs.org/logo-og.png" alt="React logo" className={styles.card_image} />
+            <p className={styles.card_name}>Sudoku</p>
+          </div>
+        </Link>
+        <Link to="/games/anagrams">
+          <div className={styles.card}>
+            <img src="https://reactjs.org/logo-og.png" alt="React logo" className={styles.card_image} />
+            <p className={styles.card_name}>Anagrams</p>
+          </div>
+        </Link>
       </div>
     </div>
   );
